87c80d339b156af0e0750979ea21dc895590f914
JohnRock <shevchenkovania@gmail.com>
2023-11-24 18:35:17 +0200

    separate style to 3 classes different

--- frontend/src/pages/Home - Copy.jsx
+++ frontend/src/pages/Home - Copy.jsx
@@ -1,18 +1,22 @@
 // React Hooks
 import { Link } from "react-router-dom";
 
+
 // Components
 import Footer from "../components/Footer";
+
 
 // Styles
 import '../styles/Home.css';
 
+
 export default function Home() {
     return (
         <div className="home">
+
             <div className="home--epigraph">
                 <h2>3:16</h2>
-                <hr />
+                <hr />                            
             </div>
 
             <div className="home--category--parent">
@@ -20,11 +24,16 @@ export default function Home() {
                     <Link
                         className="home--link dent"
                         to='stomatologie'>
+
                         <div>
+                           
                             <div className="dent--img" />
+                            
                         </div>
+                        
                     </Link>
                     <p>Pastreaza ce ti-e mai scump</p>
+                   
                 </div>
 
                 <div className="home--category farm">
@@ -32,25 +41,35 @@ export default function Home() {
                         className="home--link"
                         to='farmacie'>
                         <div>
+
                             <div className="farm--img" />
+
                         </div>
+
                     </Link>
-                    <p>Vrei sa te faci sanatos?</p>
+
+                    <p>Vrei sa te faci sanatos?</p>          
+
                 </div>
 
                 <div className="home--category beauty">
                     <Link
                         className="home--link"
                         to='beauty'>
-                        <div>
+
+                        <div>        
+
                             <div className="beauty--img" />
+
                         </div>
                     </Link>
-                    <p>Tot ceea ce este invizibil este etern.</p>
-                </div>
-            </div>
 
-            <Footer />
+                    <p>Tot  ceea ce este invizibil este etern.</p>     
+
+                </div>
+             
+              </div>
+              <Footer />
         </div>
     )
 }
